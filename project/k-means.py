00af5eaa27bafec478d6b2176d1ff63e87e349fc
Caleb Piekstra <piekstra17@up.edu>
2015-11-29 18:28:44 -0800

    Added plotting to k=2 k-means clustering
    Still needs comments!

--- project/k-means.py
+++ project/k-means.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 class KMeans:
     
@@ -29,6 +31,21 @@ class KMeans:
     def cluster (self):
         ""
     
+    def plot(self, clusters, figure):
+        # only allow plotting of clusters with
+        # genes with 2 expressions (2D graph)
+        if self.numExps != 2:
+            return
+        # remove the gene label from each gene
+        refinedClusters = [[gene[0] for gene in cluster] for cluster in clusters]
+        # modify clusters to have all x's in one tuple and all
+        # y's in the other tuple (seperate exp1 and exp2)
+        xyClusters = [list(zip(*cluster)) for cluster in refinedClusters]
+        clusterColors = colors.cnames.values()[:len(xyClusters)]
+        plt.figure(figure)
+        for idx, cluster in enumerate(xyClusters):
+            plt.scatter(cluster[0], cluster[1], color=clusterColors[idx])
+            
     def kmeans (self):
         clusters = self.newClusters()
         # for each item, randomly assign it to one of the k clusters
@@ -36,10 +53,11 @@ class KMeans:
             clusters[random.randint(0,self.k-1)].append((gene, geneIdx))
         # remove any empty clusters
         clusters = filter(None, clusters)
-
         counter = 0
         while True:
             counter += 1
+            if self.numExps == 2:
+                self.plot(clusters, counter)
             newClusters = self.newClusters(len(clusters))
             centers = [self.center(zip(*cluster)[0]) for cluster in clusters]
             if self.verbose:
@@ -62,7 +80,7 @@ class KMeans:
 # preprocessing (input)    
 exp1 = [1, 6, 5, 2, 6, 0, 1, 1, 2, 4, 5, 6, 7]
 exp2 = [4, 2, 3, 5, 4, 5, 3, 1, 2, 4, 5, 6, 7]
-exps = (exp1, exp2, exp1, exp1, exp2, exp1, exp1)
+exps = (exp1, exp2)
 
 # preliminary data
 microarrayData = [gene for gene in zip(*exps)]
@@ -76,3 +94,5 @@ print "\nFinal set of gene clusters:"
 for clusterIdx, cluster in enumerate(finalClusters):
     print "\tCluster %d: %s" % (clusterIdx+1, ["gene" + str(idx+1) for gene, idx in cluster])
 print ""
+if self.numExps == 2:
+    plt.show()
