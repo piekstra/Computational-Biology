e5da4e36efb79b7517727f144c1c2a39b0b96573
Caleb Piekstra <piekstra17@up.edu>
2015-12-11 20:00:31 -0800

    Submission version of the project

---
+++ project/README.txt
@@ -0,0 +1,18 @@
+To run the program, simply run clusterMaster.py using Python 3.X
+
+Example:
+	$ python3 clusterMaster.py
+
+To edit the k value for k-means clustering and the diameter for QT clustering
+you have to edit the 'k' and 'diameter' variable in clusterMaster.py
+
+The clustering results as well as a tree of the genes is printed out to the
+console and written to the file 'results.txt'
+
+To modify the microarray data, edit the 'microarraydata.csv' file.
+Note that the program expects that each column will be labeled with
+an expression header and each row's first column will contain the 
+gene name. The values contained in the rest of the file are comma-
+seperated integers.
+
+
--- project/clusterMaster.py
+++ project/clusterMaster.py
@@ -15,11 +15,35 @@ VERBOSE = False
 
 # the input file
 inputFile = "microarraydata.csv"
+# the output file
+outputFile = "results.txt"
 # A CSV file reader
 csvReader = CSVReader()
 
 # get the microarray data from the csv file
 microarrayData = csvReader.read(inputFile)
+
+## k-means algorithm!
+# set the k-value (max potential clusters)
+k = 6
+# holds a reference to a KMeans object 
+kmeans = KMeans(verbose=VERBOSE)
+# get the clusters determined by the algorithm
+kMeansFinalClusters = kmeans.kmeans(microarrayData, k)
+
+## QT algorithm!
+# set the diameter for the QT algorithm
+diameter = 6
+# holds a reference to a QT object 
+qt = QT(verbose=VERBOSE)
+# get the clusters determined by the algorithm
+qtFinalClusters = qt.QTClustering(microarrayData, diameter)
+
+## Hierarchical Clustering algorithm!
+# holds a reference to a QT object 
+hc = Hierarchical(microarrayData, VERBOSE)
+# get the clusters determined by the algorithm
+hcFinalClusters = hc.hierarchicalCluster()
 
 # print out the data in the microarray if in VERBOSE mode
 if VERBOSE:
@@ -27,44 +51,38 @@ if VERBOSE:
     for idx, gene in enumerate(microarrayData):
         print ("Gene %d:" % (idx+1), gene)
 
-## k-means algorithm!
-# set the k-value (max potential clusters)
-k = 6
-
-# holds a reference to a KMeans object 
-kmeans = KMeans(verbose=VERBOSE)
-# get the clusters determined by the algorithm
-kMeansFinalClusters = kmeans.kmeans(microarrayData, k)
-
-# print out the clusters
+# print out the k-means clusters
 print ("\n\nFinal sets of gene clusters:\n")
 print ("Using K-Means Algorithm:")
 for clusterIdx, cluster in enumerate(kMeansFinalClusters):
     print ("\tCluster %d: {%s}" % (clusterIdx+1, ', '.join([str(idx+1) for gene, idx in cluster])))
 print ("")
 
-## QT algorithm!
-# set the diameter for the QT algorithm
-diameter = 6
-
-# holds a reference to a QT object 
-qt = QT(verbose=VERBOSE)
-# get the clusters determined by the algorithm
-qtFinalClusters = qt.QTClustering(microarrayData, diameter)
-
-# print out the clusters
+# print out the QT clusters
 print ("Using QT Algorithm:")
 for clusterIdx, cluster in enumerate(qtFinalClusters):
     print ("\tCluster %d: {%s}" % (clusterIdx+1, ', '.join([str(geneNum+1) for geneNum in cluster])))
 print ("")
 
-## Hierarchical Clustering algorithm!
-
-# holds a reference to a QT object 
-hc = Hierarchical(microarrayData, VERBOSE)
-# get the clusters determined by the algorithm
-hcFinalClusters = hc.hierarchicalCluster()
-
-# print out the clusters
+# print out the hierarchical tree
 print ("Using Hierarchical Clustering Algorithm:")
 print ("Tree: %s" % hcFinalClusters)
+
+with open(outputFile, 'w') as out:
+    # print out the k-means clusters
+    out.write("\n\nProgram results for gene clusters:\n\n")
+    out.write("\nUsing K-Means Algorithm with k = %d:\n" % (k))
+    for clusterIdx, cluster in enumerate(kMeansFinalClusters):
+        out.write("\tCluster %d: {%s}\n" % (clusterIdx+1, ', '.join([str(idx+1) for gene, idx in cluster])))
+    out.write("")
+
+    # print out the QT clusters
+    out.write("\nUsing QT Algorithm with diameter %d:\n" % (diameter))
+    for clusterIdx, cluster in enumerate(qtFinalClusters):
+        out.write("\tCluster %d: {%s}\n" % (clusterIdx+1, ', '.join([str(geneNum+1) for geneNum in cluster])))
+    out.write("")
+
+    # print out the hierarchical tree
+    out.write("\nUsing Hierarchical Clustering Algorithm:\n")
+    out.write("Tree: %s\n" % hcFinalClusters)
+    
